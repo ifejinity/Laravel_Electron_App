30a7cab83350a699c1184d32502b46bbc7222016
Jeffrey Lonzanida <mrlonzanida08@gmail.com>
2025-03-29 14:54:28 +0800

    first commit

---
+++ electron/main.js
@@ -0,0 +1,26 @@
+import { app, BrowserWindow } from "electron";
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load the Laravel + Vue app
+  mainWindow.loadURL("http://127.0.0.1:8000");
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
